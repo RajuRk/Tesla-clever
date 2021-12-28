41dbb8f00d87751d2b7a4e7cda4dd00d91caa75e
RajuRk <45646335+RajuRk@users.noreply.github.com>
2021-12-28 22:32:49 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,59 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="Logo"/>
+      </a>
+      <Menu>
+        <p><a href="#">Model S</a></p>
+        <p><a href="#">Model 3</a></p>
+        <p><a href="#">Model X</a></p>
+        <p><a href="#">Model Y</a></p>
+        <p><a href="#">Solar Roof</a></p>
+        <p><a href="#">Solar Panels</a></p>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`; 
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a{
+    font-weight: 600;
+    text-tranform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+
+const RightMenu = styled.div`
+  a{
+    font-weight: 600;
+    text-tranform: uppercase;
+    margin-right: 10px;
+  }
+`;
